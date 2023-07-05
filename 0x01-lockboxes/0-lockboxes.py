0d119ba99604f3cc70315244504ab7835953abd0
Nnamani John <nnamanijohn123@gmail.com>
2023-07-05 23:07:01 +0100

    lock box

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    stack = [0]
+
+    while stack:
+        curr_box = stack.pop() 
+        for key in boxes[curr_box]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                stack.append(key)
+
+    return all(unlocked) 
