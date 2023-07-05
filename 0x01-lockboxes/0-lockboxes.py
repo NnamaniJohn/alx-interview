1eaae29c6de45b3337f5752567e13929d50d4744
Nnamani John <nnamanijohn123@gmail.com>
2023-07-05 23:11:59 +0100

    lock box

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,14 +1,19 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
 def canUnlockAll(boxes):
+    """ Method that determines if all the boxes can be opened. """
     n = len(boxes)
     unlocked = [False] * n
     unlocked[0] = True
     stack = [0]
 
     while stack:
-        curr_box = stack.pop() 
+        curr_box = stack.pop()
         for key in boxes[curr_box]:
             if key < n and not unlocked[key]:
                 unlocked[key] = True
                 stack.append(key)
 
-    return all(unlocked) 
+    return all(unlocked)
