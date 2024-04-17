d82a781ccc63403f1b853099640e60597cd0a18c
Nnamani John <nnamanijohn123@gmail.com>
2024-04-17 16:21:41 +0100

    star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,24 +6,24 @@ const url = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
 const characters = [];
 
 request(url, async (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        const film = JSON.parse(body);
-        for (const character of film.characters) {
-        const promise = new Promise((resolve, reject) => {
-            request(character, (error, response, body) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(JSON.parse(body).name);
-            }
-            });
+  if (error) {
+    console.log(error);
+  } else {
+    const film = JSON.parse(body);
+    for (const character of film.characters) {
+      const promise = new Promise((resolve, reject) => {
+        request(character, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
         });
-        characters.push(promise);
-        }
-        for (const character of characters) {
-        console.log(await character);
-        }
+      });
+      characters.push(promise);
     }
+    for (const character of characters) {
+      console.log(await character);
+    }
+  }
 });
